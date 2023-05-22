4a8941d082c5cbf08f572f47604b523cf206f99b
CarloBattista <carlitobatti@gmail.com>
2023-05-22 14:19:21 +0200

    container heading

--- index.php
+++ index.php
@@ -34,7 +34,16 @@
 
 <body>
 
-
+    <!-- Container -->
+    <div class="container">
+        <!-- Heading -->
+        <div class="heading mt-5 mb-5">
+            <h1>Strong Password Generator</h1>
+            <h2>Genera una password sicura</h2>
+        </div>
+        <!-- Container Main -->
+        <div class="container_main"></div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
